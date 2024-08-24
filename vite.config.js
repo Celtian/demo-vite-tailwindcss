85a2e01b650ed27f9c2fca0ed05ea71f5b531c8b
Dominik Hladík <dominik.hladik@seznam.cz>
2024-08-24 13:52:39 +0200

    chore(base): base changed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'https://github.com/Celtian/demo-vite-tailwindcss',
+  base: 'https://Celtian.github.io/demo-vite-tailwindcss',
 });
